357ef9d9bb007f9e06c574709405cd92ea3e0b37
Vladimir <govorov2001gv@gmail.com>
2022-01-15 22:40:10 +0300

    finished static DOM-structure, without props

---
+++ mini-app/src/components/app/app.js
@@ -0,0 +1,26 @@
+import './app.css';
+
+import AppInfo from '../app-info/app.info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+function App() {
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployersList/>
+            <EmployersAddForm/>
+        </div>
+    );
+}
+
+export default App;
